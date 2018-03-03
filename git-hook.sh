c7ff1d15e8c60a59a3af423f7d022196f62f894a
Jack Laxson <jackjrabbit@gmail.com>
2018-03-02 20:04:24 -0500

    run shellcheck, do stuff

--- git-hook.sh
+++ git-hook.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# Call this script with you trigger token from docker hub!
 git fetch
 git checkout 1.2.0
 git pull upstream
 git push origin
 git checkout debian-1.2
 git push origin
-http post https://registry.hub.docker.com/u/jrabbit/taskserver/trigger/$0/ build=true
+http post https://registry.hub.docker.com/u/jrabbit/taskserver/trigger/"$0"/ build=true
