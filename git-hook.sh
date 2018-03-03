7e64b2b1d72b16210012c1537c62519f98f0b252
Jack Laxson <jackjrabbit@gmail.com>
2018-03-02 20:02:51 -0500

    add githook for taskserver repo

---
+++ git-hook.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git fetch
+git checkout 1.2.0
+git pull upstream
+git push origin
+git checkout debian-1.2
+git push origin
+http post https://registry.hub.docker.com/u/jrabbit/taskserver/trigger/$0/ build=true
