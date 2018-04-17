426170f1dd5fb6d813a1d2d7693086918c27275c
Jack Laxson <jackjrabbit@gmail.com>
2018-04-17 17:27:18 -0400

    add fabfile commands to debug and push

--- fabfile.py
+++ fabfile.py
@@ -16,3 +16,12 @@ def outdated():
 def auto():
     deps()
     build()
+
+@task
+def push():
+    local("docker push jrabbit/taskd:latest")
+
+@task
+def debug():
+    local("gnutls-cli --x509keyfile pki/client.key.pem --x509certfile pki/client.cert.pem localhost -p 53589 --x509cafile ./pki/ca.cert.pem -r -V -d 99")
+    local("openssl s_client -connect localhost:53589 -cert pki/client.cert.pem -key pki/client.key.pem -CAfile pki/ca.cert.pem")
